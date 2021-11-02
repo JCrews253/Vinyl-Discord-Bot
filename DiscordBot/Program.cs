78114bc5ccdcf5b0de390e43cdd5a9aa77dda6d1
James Crews <jcrews206@gmail.com>
2021-11-02 14:18:24 -0700

    git init

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,34 @@
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      MainAsync().GetAwaiter().GetResult();
+    }
+
+    static async Task MainAsync()
+    {
+      var discord = new DiscordClient(new DiscordConfiguration()
+      {
+        Token = "NzczNzIxMTI1ODcxODEyNjIy.X6NV8w.G20JhtiqCbYeiVKNQ8yk6aei0aY",
+        TokenType = TokenType.Bot,
+        Intents = DiscordIntents.AllUnprivileged
+      });
+
+      discord.MessageCreated += async (s, e) =>
+      {
+        if (e.Message.Content.ToLower().StartsWith("ping"))
+          await e.Message.RespondAsync("pong!");
+
+      };
+
+      await discord.ConnectAsync();
+      await Task.Delay(-1);
+    }
+  }
+}
